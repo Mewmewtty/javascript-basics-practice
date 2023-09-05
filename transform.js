9978093eded42f152d7f64d6d9ee010f328760ba
Marta Camp <mcampvi@gmail.com>
2023-09-05 21:18:09 +0200

    Creación ejercicio transform

---
+++ transform.js
@@ -0,0 +1,40 @@
+const datos = [
+    {
+      id: 1,
+      nombre: 'Juan',
+      habilidades: ['JavaScript', 'HTML', 'CSS'],
+      proyectos: [
+        { id: 1, nombre: 'Proyecto 1' },
+        { id: 2, nombre: 'Proyecto 2' }
+      ]
+    },
+    {
+      id: 2,
+      nombre: 'María',
+      habilidades: ['Python', 'SQL', 'Django'],
+      proyectos: [
+        { id: 3, nombre: 'Proyecto 3' },
+        { id: 4, nombre: 'Proyecto 4' }
+      ]
+    },
+    {
+      id: 3,
+      nombre: 'Pedro',
+      habilidades: ['Java', 'Spring', 'Hibernate'],
+      proyectos: [
+        { id: 5, nombre: 'Proyecto 5' },
+        { id: 6, nombre: 'Proyecto 6' }
+      ]
+    }
+  ];
+  
+  const desarrolladoresJavascript = datos.filter(desarrollador => (
+    desarrollador.habilidades.includes('JavaScript')
+  ));
+  
+  console.log(desarrolladoresJavascript);
+  
+  const listadoProyectos = datos.map(desarrollador => (
+    desarrollador.proyectos.map(proyecto => proyecto.nombre)
+  ));
+  console.log(listadoProyectos);
