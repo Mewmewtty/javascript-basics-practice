176a85e72dc1ad0dddc5c8b3574da26ecec030d0
Marta Camp <mcampvi@gmail.com>
2023-09-05 20:22:12 +0200

    Creación ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,12 @@
+let alumno = {
+	nombre = 'Marta',
+	apellidos = 'Camp',
+	temas = ['Git', 'HTML', 'CSS', 'JavaScript'],
+	busquedaActiva = false,
+	redesSociales: [
+        {
+            red: 'GitHub',
+            enlace: 'https://github.com/Mewmewtty'
+        }
+    ]
+};
